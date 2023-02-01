9bf44d69bb9c4f616b0f92c6fb42d1cb00b6aa83
pable <pablorivasliebana@gmail.com>
2023-02-01 19:26:25 +0100

    Endpoints funcionales

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,14 @@ router.get('/', function(req, res, next) {
 router.get('/employees', function(req, res, next) {
   res.send(`<h1>Obteniendo empleados...</h1>`);
 });
-
 router.post('/employees', function(req, res, next) {
   res.send(`<h1>Creando empleados...</h1>`);
 });
-
 router.put('/employees', function(req, res, next) {
   res.send(`<h1>Actualizando empleados...</h1>`);
 });
-
 router.delete('/employees', function(req, res, next) {
   res.send(`<h1>Eliminando empleados...</h1>`);
 });
+
 module.exports = router;
