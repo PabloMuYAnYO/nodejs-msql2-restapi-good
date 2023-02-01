8c9b44a742f085ef9e306449881920e664b5faf9
pable <pablorivasliebana@gmail.com>
2023-02-01 19:26:14 +0100

    Endpoints funcionales

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
+router.get('/ping', function(req, res, next) {
+  const result = pool.query('SELECT 1 + 1 AS result');
+  res.json(result);
+});
+
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send(`<h1>Hola mundo desde express</h1>`)
+  res.send(`<h1>Hola mundo desde express</h1>`);
 });
 
 router.get('/employees', function(req, res, next) {
-  res.send(`<h1>Obteniendo empleados</h1>`)
+  res.send(`<h1>Obteniendo empleados...</h1>`);
 });
 
+router.post('/employees', function(req, res, next) {
+  res.send(`<h1>Creando empleados...</h1>`);
+});
+
+router.put('/employees', function(req, res, next) {
+  res.send(`<h1>Actualizando empleados...</h1>`);
+});
+
+router.delete('/employees', function(req, res, next) {
+  res.send(`<h1>Eliminando empleados...</h1>`);
+});
 module.exports = router;
