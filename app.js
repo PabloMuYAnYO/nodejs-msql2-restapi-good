d9771a0681352b2def32cd33cdd3e8d772e50467
= <=>
2023-02-07 10:38:39 +0100

    App lista para un despliegue

--- app.js
+++ app.js
@@ -14,8 +14,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Endpoint not found!'
+    })
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
